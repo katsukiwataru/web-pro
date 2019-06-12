69cb8547a91b91a952d322643e575d4df44786a8
katsukiwataru <a17dc050@dhw.ac.jp>
2019-06-12 16:12:26 +0900

    add tables

--- task/booklist.sql
+++ task/booklist.sql
@@ -1,10 +1,3 @@
-create table book(
-    id int primary key,
-    isbn int not null,
-    title varchar(255) not null,
-    price int(255) not null
-);
-
 create table tag(
     id int primary key,
     name varchar(255) not null
@@ -16,71 +9,25 @@ create table book_tag(
     tag_id int not null
 );
 
-// 1対1
-
-create table 日本語(
-    id int primary key,
-    word varchar(255)
-);
-
-create table 中国語(
-    id int primary key,
-    日本語_id int not null unique,
-    word varchar(255),
-    foreign key (日本語_id)
-        references 日本語(id)
-);
-
-create table 日本語(
-    id int primary key,
-    word varchar(255)
-);
-
-create table 中国語(
-    id int primary key,
-    word varchar(255)
-);
-
-create table 日本語_中国語(
-    id int primary key,
-    日本語_id int not null unique,
-    中国語_id int not null unique
-);
-
-// 外部キーを使う例
-
-// 学部
-create table faculty(
-    id int auto_increment not null primary key,
-    name varchar(255) not null);
-
-// 学生
-create table students
-    (id int auto_increment not null primary key,
-        faculty_id int not null,
-        code char(8) not null,
-        name varchar(255) not null,
-        grade int not null,
-        foreign key (faculty_id)
-        references faculty(id)
-    );
-
-
-insert into faculty(name) values('経済学部');
-insert into faculty(name) values('理工学部');
-
-// 1対多
-// 親子関係
-
-create table メーカー(
+create table category(
     id int primary  key,
     name varchar(255)
 );
 
-create table 商品(
+create table author(
+    id int primary  key,
+    name varchar(255)
+);
+
+create table book(
     id int primary key,
-    name varchar(255),
-    メーカー_id int not null,
-    foreign key (メーカー_id)
-        references メーカー(id)
+    isbn int not null,
+    title varchar(255) not null,
+    price int(255) not null,
+    category_id int not null,
+    foreign key (category_id)
+        references category(id)
+    author_id int not null
+    foreign key (author_id)
+        references author(id)
 );
